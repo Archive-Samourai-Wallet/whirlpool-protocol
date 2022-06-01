18d298df035f900c59d97986edb51608366a5307
zeroleak <zeroleak@samourai.io>
2022-06-01 16:25:55 +0200

    add RegisterOutputRequest.bordereau64 to allow retrying REGISTER_OUTPUT with same bordereau but different address

--- src/main/java/com/samourai/whirlpool/protocol/rest/RegisterOutputRequest.java
+++ src/main/java/com/samourai/whirlpool/protocol/rest/RegisterOutputRequest.java
@@ -4,13 +4,18 @@ public class RegisterOutputRequest {
   public String inputsHash;
   public String unblindedSignedBordereau64;
   public String receiveAddress;
+  public String bordereau64; // since 0.23.9 (before, it was based on receiveAddress)
 
   public RegisterOutputRequest() {}
 
   public RegisterOutputRequest(
-      String inputsHash, String unblindedSignedBordereau64, String receiveAddress) {
+      String inputsHash,
+      String unblindedSignedBordereau64,
+      String receiveAddress,
+      String bordereau64) {
     this.inputsHash = inputsHash;
     this.unblindedSignedBordereau64 = unblindedSignedBordereau64;
     this.receiveAddress = receiveAddress;
+    this.bordereau64 = bordereau64;
   }
 }
