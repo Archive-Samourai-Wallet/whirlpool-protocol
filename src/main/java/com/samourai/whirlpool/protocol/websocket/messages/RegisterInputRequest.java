376ee623767e27208be25910a4c917867ce34c6d
zeroleak <zeroleak@samourai.io>
2022-06-01 18:30:34 +0200

    add RegisterInputRequest.blockHeight

--- src/main/java/com/samourai/whirlpool/protocol/websocket/messages/RegisterInputRequest.java
+++ src/main/java/com/samourai/whirlpool/protocol/websocket/messages/RegisterInputRequest.java
@@ -7,15 +7,22 @@ public class RegisterInputRequest {
 
   public String signature;
   public boolean liquidity;
+  public int blockHeight = 0; // zero for protocol < 0.23.9
 
   public RegisterInputRequest() {}
 
   public RegisterInputRequest(
-      String poolId, String utxoHash, long utxoIndex, String signature, boolean liquidity) {
+      String poolId,
+      String utxoHash,
+      long utxoIndex,
+      String signature,
+      boolean liquidity,
+      int blockHeight) {
     this.poolId = poolId;
     this.utxoHash = utxoHash;
     this.utxoIndex = utxoIndex;
     this.signature = signature;
     this.liquidity = liquidity;
+    this.blockHeight = blockHeight;
   }
 }
